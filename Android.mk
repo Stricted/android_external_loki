dcca6688a7c4e05e82e58738aafef8a46833c81e
Jan Altensen <info@stricted.net>
2019-08-10 11:00:16 +0200

    loki: ignore warnings
    Change-Id: I5dddd1db265ad11663883a9b4413ecf5ca166510

--- Android.mk
+++ Android.mk
@@ -20,6 +20,7 @@ include $(BUILD_EXECUTABLE)
 
 # build host binary
 include $(CLEAR_VARS)
+LOCAL_CFLAGS := -Wno-pointer-arith -Wno-unused-result -Wno-sign-compare
 LOCAL_SRC_FILES := loki_flash.c loki_patch.c loki_find.c loki_unlok.c main.c
 LOCAL_MODULE := loki_tool
 include $(BUILD_HOST_EXECUTABLE)
